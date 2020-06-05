f6bf6a0f7214f58314a71006b74958038d5b6082
nvjot94 <nvjot94@gmail.com>
2020-06-05 11:20:43 -0400

    final

--- src/components/restaurant/restaurantList.js
+++ src/components/restaurant/restaurantList.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import { connect } from 'react-redux'; 
+import { connect } from 'react-redux';
 import Restaurant from './restaurant';
 import './restaurant.css';
 
 const RestaurantList = ({ data, filtered }) => {
-
-
 	return (
 		<div className='project-div'>
 			<div>
-				{data||filtered?<h2>Total Results : {filtered?filtered.length:data.length}</h2>:''}
+				{data || filtered ? (
+					<h2>
+						Total Results :{' '}
+						{filtered ? filtered.length : data.length}
+					</h2>
+				) : (
+					''
+				)}
 			</div>
-			hello
-			<div className='project-container' data-testid='restaurant-component'>
-			{filtered ? (
-						filtered.map(item => (
-							<Restaurant restaurant={item} key={item.id}  />
-						))
-					) : data ? (
-						data.map(item => (
-							<Restaurant restaurant={item} key={item.id}  />
-						))
-					) : null}
+			<div
+				className='project-container'
+				data-testid='restaurant-component'
+			>
+				{filtered ? (
+					filtered.map(item => (
+						<Restaurant restaurant={item} key={item.id} />
+					))
+				) : data ? (
+					data.map(item => (
+						<Restaurant restaurant={item} key={item.id} />
+					))
+				) : null}
 			</div>
 		</div>
 	);
@@ -32,6 +39,5 @@ const mapStateToProps = state => ({
 	filtered: state.restaurants.filtered,
 	isFetching: state.restaurants.isFetching
 });
-
 
 export default connect(mapStateToProps)(RestaurantList);
